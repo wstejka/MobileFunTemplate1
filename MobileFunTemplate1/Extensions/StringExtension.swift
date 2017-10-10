c8afea029c80fba0bb29a2cfcb30e7df16e73dee
Wojciech S <wstejka@gmail.com>
2017-10-10 17:27:17 +0200

    BG-146: Add Localization files

---
+++ MobileFunTemplate1/Extensions/StringExtension.swift
@@ -0,0 +1,96 @@
+//
+//  String+Extension.swift
+//  CheckFuelPrice
+//
+//  Created by Wojciech Stejka on 30/03/2017.
+//  Copyright © 2017 Wojciech Stejka. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    func localized(withDefaultValue:String) -> String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: withDefaultValue, comment: "")
+    }
+
+    func localized(withComment:String) -> String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "test", comment: withComment)
+    }
+
+    func localized() -> String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+    }
+    
+}
+
+extension String {
+    
+    func throwException(_ code : Int = 1) -> NSError {
+        
+        let userInfo = ["error" : self]
+        let domain = String(#function) + ":" + String(#line)
+        return NSError(domain: domain, code: code, userInfo: userInfo);
+    }
+
+}
+
+extension String {
+
+    var length : Int{
+        return self.characters.count
+    }
+    // string[] -> 'character'
+    subscript (i: Int) -> String? {
+        
+        return ((i < self.length) ? self[Range(i ..< i + 1)] : nil)
+    }
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[Range(start ..< end)])
+    }
+    subscript (r: CountableClosedRange<Int>) -> String {
+        let startIndex =  self.index(self.startIndex, offsetBy: r.lowerBound)
+        let endIndex = self.index(startIndex, offsetBy: r.upperBound - r.lowerBound)
+        return String(self[startIndex...endIndex])
+    }
+}
+
+
+extension String {
+    
+    func capitalizingFirstLetter() -> String {
+        let first = String(characters.prefix(1)).capitalized
+        let other = String(characters.dropFirst())
+        return first + other
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
+
+// Update for Swift 3 (Xcode 8):
+// https://gist.github.com/robnadin/2720534f91702c444b6b9bde0fdfe224
+// Cast Range<String.Index> to NSRange
+extension String {
+    func nsRange(from range: Range<String.Index>) -> NSRange {
+        let from = range.lowerBound.samePosition(in: utf16)
+        let to = range.upperBound.samePosition(in: utf16)
+        return NSRange(location: utf16.distance(from: utf16.startIndex, to: from!),
+                       length: utf16.distance(from: from!, to: to!))
+    }
+    
+    func range(from nsRange: NSRange) -> Range<String.Index>? {
+        guard
+            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+            let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
+            let from = String.Index(from16, within: self),
+            let to = String.Index(to16, within: self)
+            else { return nil }
+        return from ..< to
+    }
+}
